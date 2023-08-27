37882b62fc44e10926f6fd17a42727191076649a
melissageoghegan <melissageoghegan95@gmail.com>
2023-08-27 23:08:38 +0100

    added screens to app

--- App.js
+++ App.js
@@ -4,31 +4,30 @@ import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 
 const App = createNativeStackNavigator({
-  Products: {screen: ProductsScreen},
-  Tutorials: {screen: TutorialsScreen},
+    Products: {screen: ProductsScreen},
+    Tutorials: {screen: TutorialsScreen},
 });
 const Stack = createNativeStackNavigator();
 const Logo = require('./assets/PNGLogo.png');
 
 export default function App() 
-  
-  return ({
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen
-          name="Products"
-          component={ProductsScreen}
-          options={{title: 'Products'}}
-        />
-        <Stack.Screen
-        name="Tutorials"
-        component={TutorialsScreen}
-        options={{title: 'Tutorials'}}
-        />
-      </Stack.Navigator>
-    </NavigationContainer>
-  );
 
+return ({
+    <NavigationContainer>
+        <Stack.Navigator>
+            <Stack.Screen
+                name="Products"
+                component={ProductsScreen}
+                options={{title: 'Products'}}
+            />
+            <Stack.Screen
+                name="Tutorials"
+                component={TutorialsScreen}
+                options={{title: 'Tutorials'}}
+            />
+        </Stack.Navigator>
+    </NavigationContainer>
+);
 
 const styles = StyleSheet.create({
   container: {
