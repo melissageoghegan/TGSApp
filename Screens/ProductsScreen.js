2a22f2ddb81105410e44437bd6b21a5c939fe168
melissageoghegan <melissageoghegan95@gmail.com>
2023-08-30 13:15:06 +0100

    added test text to product screen

--- Screens/ProductsScreen.js
+++ Screens/ProductsScreen.js
@@ -3,9 +3,10 @@ import { StyleSheet, Text, View, Image } from 'react-native';
 function ProductsScreen ({navigation}){
     return(
         <View>
-            <Text>
-                Yes please
-            </Text>
+            <Text>Nope. The dog is too smelly.</Text>
+            <Text>Just like her grandad Wally</Text>
+            <Text>bad dog</Text>
+            <Text>bold</Text>
         </View>
     );
 }
